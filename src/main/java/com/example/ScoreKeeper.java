424829cea978019966beaaf4419c512ce448a899
Manuel Stahl <manuel.stahl@wheego-mobility.com>
2023-06-06 22:35:13 +0200

    ScoreKeeper class functions implemented

--- src/main/java/com/example/ScoreKeeper.java
+++ src/main/java/com/example/ScoreKeeper.java
@@ -2,28 +2,31 @@ package com.example;
 
 public class ScoreKeeper {
 
-    private int score = 0;
+    private int scoreA = 0;
+    private int scoreB = 0;
 
 
     void scoreTeamA1() {
-
+        scoreA ++;
     }
     void scoreTeamA2(){
-
+        scoreA += 2;
     }
     void scoreTeamA3(){
-
+        scoreA += 3;
     }
     void scoreTeamB1(){
-
+        scoreB++;
     }
     void scoreTeamB2(){
-
+        scoreB += 2;
     }
     void scoreTeamB3(){
-
+        scoreB += 3;
     }
     String getScore(){
-    return "";
+    return String.format("%03d", scoreA)
+            .concat(":")
+            .concat(String.format("%03d", scoreB));
     }
 }
